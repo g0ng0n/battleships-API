1fab4b4dd8464a7cf258d807263fca1ce98df549
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2016-09-07 23:25:53 -0300

    modified the variable user for player, and also added the create_game endpoint

--- battleships-api.py
+++ battleships-api.py
@@ -2,27 +2,30 @@ import logging
 import endpoints
 import random
 import gameutils
-from models import User, StringMessage
+from models import Player, Game
+from models import GameForm, StringMessage
 from protorpc import remote, messages
 from google.appengine.ext import ndb
 import re
 
 
-USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
+PLAYER_REQUEST = endpoints.ResourceContainer(player_name=messages.StringField(1),
                                            email=messages.StringField(2))
+
+GAME_REQUESTS = endpoints.ResourceContainer(player_name=messages.StringField(1))
 
 
 
 @endpoints.api(name='battleships', version='v1')
 class BattleshipApi(remote.Service):
-    @endpoints.method(request_message=USER_REQUEST,
+    @endpoints.method(request_message=PLAYER_REQUEST,
                       response_message=StringMessage,
-                      path='user',
-                      name='create_user',
+                      path='player',
+                      name='create_player',
                       http_method='POST')
-    def create_user(self, request):
-        """Create a User. Requires a unique username"""
-        if User.query(User.name == request.user_name).get():
+    def create_player(self, request):
+        """Create a User. Requires a unique playername"""
+        if Player.query(Player.name == request.player_name).get():
             raise endpoints.ConflictException(
                 'A User with that name already exists!')
 
@@ -31,11 +34,36 @@ class BattleshipApi(remote.Service):
             raise endpoints.ConflictException(
                 'invalid email, please try again!')
 
-        user = User(name=request.user_name, email=request.email, board=User.create_empty_board())
-        user.put()
+        player = Player(name=request.player_name, email=request.email, board=Player.create_empty_board())
+        player.put()
 
-        return StringMessage(message='User created!'.format(
-            request.user_name))
+        return StringMessage(message='Player created!'.format(request.player_name))
+
+    @endpoints.method(request_message=GAME_REQUESTS,
+                    response_message=GameForm,
+                    path='game',
+                    name='create_game',
+                    http_method='POST')
+    def create_game(self, request):
+        """One of the players, creates the game and gets the game-id and gives that ID
+        to the other player in order to play between each other"""
+        player = Player.query(Player.name == request.player_name).get()
+        if not player:
+            raise endpoints.NotFoundException(
+                'A Player with that name does not exist!, '
+                'we need one player in order to create the game')
+        try:
+            game = Game.new_game(player)
+        except ValueError:
+            raise endpoints.BadRequestException('sarasa')
+
+        # Use a task queue to update the average attempts remaining.
+        # This operation is not needed to complete the creation of a new game
+        # so it is performed out of sequence.
+
+        return game.to_form('Game created!, we only need one player '
+                            'to join in order to start the game')
+
 
 
 api = endpoints.api_server([BattleshipApi])
--- models.py
+++ models.py
@@ -5,11 +5,12 @@ from google.appengine.ext import ndb
 import random
 
 
-class User(ndb.Model):
+class Player(ndb.Model):
     """User profile"""
     name = ndb.StringProperty(required=True)
-    email =ndb.StringProperty()
+    email = ndb.StringProperty()
     board = ndb.PickleProperty(required=True, default=[])
+    moves = ndb.IntegerProperty()
 
     @classmethod
     def create_empty_board(cls):
@@ -23,6 +24,41 @@ class User(ndb.Model):
 
         return grid
 
+
+class Game(ndb.Model):
+    """Game object"""
+    history = ndb.PickleProperty(required=True, default=[])
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    player1 = ndb.KeyProperty(required=True, kind='Player')
+    player2 = ndb.KeyProperty(required=False, kind='Player')
+
+    @classmethod
+    def new_game(cls, player):
+        """Creates and returns a new game"""
+
+        game = Game(player1=player.key,
+                    history=[],
+                    game_over=False)
+        game.put()
+        return game
+
+
+    def to_form(self, message):
+        """Returns a GameForm representation of the Game"""
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.player_name = self.player1.get().name
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
     message = messages.StringField(1, required=True)
+
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    message = messages.StringField(4, required=True)
+    player_name = messages.StringField(5, required=True)
